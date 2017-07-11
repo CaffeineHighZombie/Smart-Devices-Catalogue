1c130b5de4be46cfde21fea79eb3c99cc95b866a
Saranyaraj Rajendran <rs.saranyaraj@gmail.com>
2017-07-11 14:48:13 +0530

    Amazon-Scraping.py - change sha to hashlib library and use sha256 to get 44 bit digest. Use entire url as data.

--- Amazon-Scraping.py
+++ Amazon-Scraping.py
@@ -1,18 +1,22 @@
 #!usr/bin/env python
 
-import hmac, sha, base64
+import hmac, hashlib, base64
 
 class AWSSigner:
 
 	def __init__(self, secret_key):
 		self.secret_key = secret_key
 
-	def sign(self, service, operation, timestamp):
-		h = hmac.new(self.secret_key, str(service) + str(operation) + str(timestamp), sha)
+	#def sign(self, service, operation, timestamp):
+	def sign(self, data):
+		#h = hmac.new(self.secret_key, str(service) + str(operation) + str(timestamp), hashlib.sha256)
+		h = hmac.new(self.secret_key, data, hashlib.sha256)
 		digest = h.digest()
 		return base64.b64encode(digest)
 
 AWSRequest = AWSSigner('QcfpCpzijMjttVfdwcokL2Q5NcDWpZRRShHP0KgL')
 
-print(AWSRequest.sign("AWSECommerceService", "ItemSearch", "2017-07-11T04:35:51.000Z"))
+data = """http://webservices.amazon.com/onca/xml?AWSAccessKeyId=AKIAINYUQ3YAQIKQONCA&AssociateTag=saranyaraj432-20&Keywords=Microsoft&Operation=ItemSearch&ResponseGroup=Images%2CItemAttributes%2COffers&SearchIndex=Software&Service=AWSECommerceService&Timestamp=2017-07-11T04%3A35%3A51.000Z"""
+
+print(AWSRequest.sign(data))
 
