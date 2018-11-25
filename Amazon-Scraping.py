62ff5d0e2578704f10c46ecb4e14140ab13248a9
Saranyaraj Rajendran <CaffeineHighZombie@users.noreply.github.com>
2018-11-25 14:57:39 +0530

    Inadvertently exposed AWS account key - Removed from Amazon-Scraping.py
    Exposed the aws account key. Removed from the code. And also, from AWS account.

--- Amazon-Scraping.py
+++ Amazon-Scraping.py
@@ -17,21 +17,12 @@ class AWSSigner:
 		digest = h.digest()
 		return base64.b64encode(digest)
 
-AWSRequest = AWSSigner('QcfpCpzijMjttVfdwcokL2Q5NcDWpZRRShHP0KgL')
-
-
-# data = """GET
-# webservices.amazon.com
-# /onca/xml
-# AWSAccessKeyId=AKIAINYUQ3YAQIKQONCA&AssociateTag=saranyaraj432-20&Keywords=Microsoft&Operation=ItemSearch&ResponseGroup=Images%2CItemAttributes%2COffers&SearchIndex=Software&Service=AWSECommerceService&Timestamp=2017-07-11T12%3A15%3A51.000Z"""
-
-# print(AWSRequest.sign(data))
-
+AWSRequest = AWSSigner('****')
 
 timestamp = strftime("%Y-%m-%dT%H:%M:%S.000Z", gmtime())
 data={}
 
-data['AWSAccessKeyId']="AKIAINYUQ3YAQIKQONCA"
+data['AWSAccessKeyId']="*****"
 data['AssociateTag']="saranyaraj432-20"
 data['Keywords']="Thermostat"
 data['Operation']="ItemSearch"
